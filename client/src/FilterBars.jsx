6b1037dd29a0bc13c4ac2f2b16c73e5b8d2f14e0
Phil Teves <phillip.teves@gmail.com>
2021-03-31 18:31:34 -0400

    quantitative descriptions added for filter bars

--- client/src/FilterBars.jsx
+++ client/src/FilterBars.jsx
@@ -32,39 +32,47 @@ class FilterBars extends React.Component {
       <div id='filter-bars'>
         <div className='bar-box'>
           <div className='bar' onClick={()=>{this.update('contrast', 'insert value')}}>
-            <div className='bar-position' style={{top: 100 - (this.state.contrast / 10) + '%'}}></div>
+            <div className='bar-position' style={{top: 100 - (this.state.contrast / 5) + '%'}}></div>
           </div>
           <span className='bar-desc'>Contrast</span>
+          <span>{this.state.contrast}%</span>
+
         </div>
         <div className='bar-box'>
           <div className='bar' onClick={()=>{this.update('h', 'insert value')}}>
             <div className='bar-position' style={{top: 100 - (this.state.h / 3.6) + '%'}}></div>
           </div>
           <span className='bar-desc'>Hue</span>
+          <span>{this.state.h}</span>
         </div>
         <div className='bar-box'>
           <div className='bar' onClick={()=>{this.update('s', 'insert value')}}>
             <div className='bar-position' style={{top: 100 - (this.state.s / 4) + '%'}}></div>
           </div>
           <span className='bar-desc'>Saturation</span>
+          <span>{this.state.s}%</span>
         </div>
         <div className='bar-box'>
           <div className='bar' onClick={()=>{this.update('l', 'insert value')}}>
             <div className='bar-position' style={{top: 100 - (this.state.l / 2) + '%'}}></div>
           </div>
           <span className='bar-desc'>Lightness</span>
+          <span>{this.state.l}%</span>
+
         </div>
         <div className='bar-box'>
           <div className='bar' onClick={()=>{this.update('a', 'insert value')}}>
             <div className='bar-position' style={{top: 100 - this.state.a + '%'}}></div>
           </div>
           <span className='bar-desc'>Opacity</span>
+          <span>{this.state.a}%</span>
         </div>
         <div className='bar-box'>
           <div className='bar' onClick={()=>{this.update('blur', 'insert value')}}>
             <div className='bar-position' style={{top: 100 - (this.state.blur * 20) + '%'}}></div>
           </div>
           <span className='bar-desc'>Blur</span>
+          <span>{this.state.blur}px</span>
         </div>
       </div>
     </div>);
