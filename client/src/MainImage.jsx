f43509cb89bbe7bb4dff1fb49889c1ddf87afaa3
Phil Teves <phillip.teves@gmail.com>
2021-03-31 21:16:59 -0400

    Image saver implemented & added - filter-based img adjustments still needed

--- client/src/MainImage.jsx
+++ client/src/MainImage.jsx
@@ -10,7 +10,7 @@ class MainImage extends React.Component {
     return(
       <div id='main-img-container'>
         <img ref={this.componentRef} id='main-img' src={this.props.image} />
-        <button onClick={()=>{exportComponentAsPNG(this.componentRef)}}>Save Image</button>
+        <button id='save-img-btn' onClick={()=>{exportComponentAsPNG(this.componentRef)}}>Save Image</button>
       </div>
     );
   }
